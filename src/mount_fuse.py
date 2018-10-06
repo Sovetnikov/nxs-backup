e1e9923bff76f134626070abccbef8973e348fae
sovetnikov <asovetnikov@gmail.com>
2018-10-06 16:59:38 +0300

    retesting

--- src/mount_fuse.py
+++ src/mount_fuse.py
@@ -7,7 +7,7 @@ import re
 import general_function
 
 mount_point = ''
-s3fs_passwd_file_path = None
+
 
 class MountError(Exception):
     def __init__(self, message):
@@ -118,7 +118,6 @@ def get_mount_data(current_storage_data):
     '''
 
     global mount_point
-    global s3fs_passwd_file_path
     dist = general_function.get_dist()
     pre_install_cmd = ''
     pre_mount = {}
@@ -205,16 +204,6 @@ def get_mount_data(current_storage_data):
             mount_cmd = '{mount_cmd} {s3fs_opts}'.format(**locals())
         if s3fs_access_key_id and s3fs_secret_access_key:
             pre_mount['check_s3fs_secrets'] = '{bucket_name}:{s3fs_access_key_id}:{s3fs_secret_access_key}'.format(**locals())
-        #     from tempfile import NamedTemporaryFile
-        #     with NamedTemporaryFile(mode='w', delete=False) as f:
-        #         f.write(bucket_name + ':' + s3fs_access_key_id + ':' + s3fs_secret_access_key)
-        #         f.close()
-        #         s3fs_passwd_file_path = f.name
-        # if s3fs_passwd_file_path:
-        #     mount_cmd = '{mount_cmd} -o passwd_file={s3fs_passwd_file_path}'.format(s3fs_passwd_file_path=s3fs_passwd_file_path, **locals())
-        import log_and_mail
-        import config
-        log_and_mail.writelog('INFO', 's3fs mount cmd: "{mount_cmd}"'.format(**locals()), config.filelog_fd)
     else:
         mount_point = ''
         return [dict_mount_data, pre_mount]
@@ -312,7 +301,6 @@ def mount(current_storage_data):
 
 
 def unmount():
-    global s3fs_passwd_file_path
     if mount_point:
         umount_cmd = "fusermount -uz %s" % (mount_point)
         umount = general_function.exec_cmd(umount_cmd)
@@ -325,10 +313,6 @@ def unmount():
             raise general_function.MyError("Bad result code external process '%s':'%s'" % (umount_cmd, code))
         else:
             general_function.del_file_objects('', mount_point)
-    if s3fs_passwd_file_path:
-        os.remove(s3fs_passwd_file_path)
-        s3fs_passwd_file_path = None
-    return 1
 
 
 def check_secrets(str_auth):
@@ -348,6 +332,7 @@ def check_secrets(str_auth):
 
     return 1
 
+
 def check_s3fs_secrets(str_auth):
     conf_path = '/etc/passwd-s3fs'
 
@@ -366,4 +351,3 @@ def check_s3fs_secrets(str_auth):
     except OSError:
         pass
     return 1
-
