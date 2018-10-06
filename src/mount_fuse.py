6ef592e09f8e16259bcb373597a7f909f90677b1
sovetnikov <asovetnikov@gmail.com>
2018-10-06 15:33:35 +0300

    s3fs auth settings

--- src/mount_fuse.py
+++ src/mount_fuse.py
@@ -209,7 +209,8 @@ def get_mount_data(current_storage_data):
         if s3fs_passwd_file_path:
             mount_cmd = '{mount_cmd} -o passwd_file={s3fs_passwd_file_path}'.format(**locals())
         import log_and_mail
-        log_and_mail.writelog('INFO', 's3fs mount cmd: "{mount_cmd}"'.format(**locals()))
+        import config
+        log_and_mail.writelog('INFO', 's3fs mount cmd: "{mount_cmd}"'.format(**locals()), config.filelog_fd)
     else:
         mount_point = ''
         return [dict_mount_data, pre_mount]
